07f101020d4f7311c345663a826d7dc68e2ad8ec
TamaiK <tamaikohsuke1118@gmail.com>
2021-11-22 12:05:42 +0900

    課題２までを作成

--- src/App.java
+++ src/App.java
@@ -1,5 +1,58 @@
+import java.util.List;
+
+import myclass.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    // 定数
+
+    private static final String MESSAGE_FORMAT_FOR_SEPARATION = "-----課題%d---------------";
+
+    public static void main(String[] args) {
+        
+        String inpuString;
+        List<String> lines;
+        
+        dispTask(1);
+        inpuString = "１行目。\n２行目。\n３行目。\n４行目。\n\n５行目\n";
+        lines = splitWithLineBreakCode(inpuString);
+        for (String line : lines) {
+            println(line);
+        }
+
+        dispTask(2);
+        inpuString = "１行目。２行目。\n３行目。４行目。\n\n５行目。";
+        lines = splitWithLineBreakCodeAndPeriod(inpuString);
+        for (String line : lines) {
+            println(line);
+        }
+    }
+
+    // 課題１
+
+    private static List<String> splitWithLineBreakCode(String inpuString) {
+
+        List<String> splittedLines = StringsSplitter.splitWithLineBreakCode(inpuString);
+        return splittedLines;
+    }
+
+    // 課題2
+
+    private static List<String> splitWithLineBreakCodeAndPeriod(String inpuString) {
+        
+        List<String> splittedLines = StringsMoreSplitter.splitWithLineBreakCodeAndPeriod(inpuString);
+        return splittedLines;
+    }
+
+    // 共通関数
+
+    private static void dispTask(int num) {
+        println(MESSAGE_FORMAT_FOR_SEPARATION, num);
+    }
+
+    // 汎用関数
+
+    private static void println(String str, Object... args) {
+        System.out.println(String.format(str, args));
     }
 }
---
+++ src/myclass/StringsMoreSplitter.java
@@ -0,0 +1,77 @@
+package myclass;
+
+import java.util.List;
+
+public class StringsMoreSplitter extends StringsSplitter {
+    
+    protected static final char CHAR_PERIOD = '。';
+
+    public static List<String> splitWithLineBreakCodeAndPeriod(String inpuString) {
+
+        StringsMoreSplitter stringsSplitter = new StringsMoreSplitter();
+
+        List<String> splittedLines = stringsSplitter.splitWithLine(inpuString);
+
+        return splittedLines;
+    }
+
+    @Override
+    protected int getSplitCharIndex(String inpuString, int beginIndex) {
+
+        int nextEndIndex;
+        int nextBreakCode = getCharIndex(inpuString, CHAR_BREAKCODE, beginIndex);
+        int nextPeriod = getCharIndex(inpuString, CHAR_PERIOD, beginIndex);
+
+        if (isFasterBreakCode(nextBreakCode, nextPeriod)) {
+            nextEndIndex = nextBreakCode;
+            return nextBreakCode;
+        }
+
+        nextEndIndex = nextPeriod + NEXT_INDEX;
+        return nextEndIndex;
+    }
+
+    protected final boolean isFasterBreakCode(int nextBreakCode, int nextPeriod) {
+        return nextBreakCode < nextPeriod;
+    }
+
+    @Override
+    protected int getNextStartIndex(int endIndex, String inpuString) {
+
+        if (isReadEnd(endIndex)) {
+            return READ_ENDED;
+        }
+
+        int nextStart = endIndex;
+
+        if (isOverIndex(endIndex, inpuString)) {
+
+            if(isPeriodBreak(endIndex, inpuString)){
+                return nextStart;
+            }
+
+            return READ_ENDED;
+        }
+
+        if (getChar(inpuString, endIndex) == CHAR_BREAKCODE) {
+            nextStart += NEXT_INDEX;
+        }
+
+        return nextStart;
+    }
+
+    protected final boolean isPeriodBreak(int endIndex, String inpuString) {
+
+        int beforeIndex = endIndex - NEXT_INDEX;
+
+        if (isOverIndex(beforeIndex, inpuString)) {
+            return false;
+        }
+
+        return getChar(inpuString, beforeIndex) == CHAR_PERIOD;
+    }
+
+    protected final char getChar(String inpuString, int endIndex) {
+        return inpuString.charAt(endIndex);
+    }
+}
